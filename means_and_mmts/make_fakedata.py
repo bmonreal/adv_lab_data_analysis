00b6ae6eb9d176d49c7c8c362c444cf949081181
Benjamin Monreal <benmonreal@gmail.com>
2020-03-27 11:33:34 -0400

    spectrometer data noise increased to desired value

--- means_and_mmts/make_fakedata.py
+++ means_and_mmts/make_fakedata.py
@@ -3,6 +3,6 @@ wavelength = 626.34
 constant = np.array([(3050+0.6*np.cos(np.pi*i/40.0))*(1/wavelength) for i in range(30)])
 
 exactdata = constant*wavelength
-realdata = np.random.normal(exactdata,0.1/constant)
-errorbar = 0.1/constant
+errorbar = 0.1*constant
+realdata = np.random.normal(exactdata,errorbar)
 runnumber = np.array(range(30))
